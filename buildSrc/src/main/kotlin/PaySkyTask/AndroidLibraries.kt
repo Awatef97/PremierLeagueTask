a8970d081719d08b27a23d7a1b9f173316f582e7
awatef97 <awatef.ahmed97@gmail.com>
2023-07-04 18:32:45 +0300

    init project

---
+++ buildSrc/src/main/kotlin/PaySkyTask/AndroidLibraries.kt
@@ -0,0 +1,43 @@
+
+object NavigationComponent{
+    const val navigationFragmentKTX = "androidx.navigation:navigation-fragment-ktx:2.5.3"
+    const val navigationUIKTX = "androidx.navigation:navigation-ui-ktx:2.5.3"
+
+}
+
+object Hilt{
+    const val hiltAndroid = "com.google.dagger:hilt-android:2.45"
+    const val hiltCompiler = "com.google.dagger:hilt-android-compiler:2.45"
+}
+
+object Coroutines{
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
+}
+object Lifecycle
+{
+    const val coreLifeCycle = "androidx.core:core-ktx:1.8.0"
+    const val fragmentLifeCycle = "androidx.fragment:fragment-ktx:1.5.1"
+    const val activityLifeCycle = "androidx.activity:activity-ktx:1.5.1"
+    const val runtimeLifeCycle = "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1"
+    const val livedataLifeCycle = "androidx.lifecycle:lifecycle-livedata-ktx:2.5.1"
+    const val viewModelLifeCycle = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1"
+
+}
+
+object Retrofit{
+    const val retrofit = "com.squareup.retrofit2:retrofit:2.9.0"
+    const val retrofitConverter = "com.squareup.retrofit2:converter-gson:2.9.0"
+    const val logInterceptor =  "com.squareup.okhttp3:logging-interceptor:4.9.0"
+}
+object Utils{
+    const val timber = "com.jakewharton.timber:timber:5.0.1"
+    const val cameraKit = "com.github.sdhoya0321:CameraKit-Android:v0.12.1-beta1"
+}
+
+object AndroidUI{
+    const val appCompact = "androidx.appcompat:appcompat:1.4.2"
+    const val materialDesign = "com.google.android.material:material:1.6.1"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val recycleView = "androidx.recyclerview:recyclerview:1.3.0"
+}
+
